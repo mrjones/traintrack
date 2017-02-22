702f64a954d8faa41513724d6533da78322819b2
Matt Jones <jonesmr@gmail.com>
2017-02-22 02:26:35 +0000

    Initial hello world, with protos

---
+++ genproto.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Get protc from: https://github.com/google/protobuf/releases
+# sudo apt-get install protobuf-compiler
+~/src/bin/protoc --proto_path ./proto/ --plugin ~/.cargo/bin/protoc-gen-rust --rust_out ./src/ proto/gtfs-realtime.proto proto/nyct-subway.proto
