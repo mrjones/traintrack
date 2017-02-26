bd96e454cd46742e3a1e7c929e33b1079073ea7b
Matt Jones <jonesmr@gmail.com>
2017-02-26 16:19:57 +0000

    Break some exploratory code into utils.rs so it can be used by server

--- src/main.rs
+++ src/main.rs
@@ -11,45 +11,7 @@ mod gtfs_realtime;
 mod result;
 mod server;
 mod stops;
-
-#[derive(Debug)]
-enum Direction {
-    UPTOWN,
-    DOWNTOWN,
-}
-
-fn infer_direction_for_trip_id(trip_id: &str) -> Direction {
-    // TODO(mrjones): Read the NYCT extension and determine this properly
-    let trip_id: String = trip_id.to_string();
-    let lastchar: String = trip_id.chars().rev().take(1).collect();
-    return match lastchar.as_ref() {
-        "N" => Direction::UPTOWN,
-        "S" => Direction::DOWNTOWN,
-        chr => panic!("Unexpcted direction {}", chr),
-    }
-}
-
-fn upcoming_trains(route: &str, stop_id: &str, feed: &gtfs_realtime::FeedMessage) -> Vec<(Direction, chrono::datetime::DateTime<chrono::UTC>)> {
-    let mut upcoming = Vec::new();
-    for entity in feed.get_entity() {
-        if entity.has_trip_update() {
-            let trip_update = entity.get_trip_update();
-            let trip = trip_update.get_trip();
-            if trip.get_route_id() == route {
-                for stop_time_update in trip_update.get_stop_time_update() {
-                    if stop_time_update.get_stop_id() == stop_id {
-                        upcoming.push((
-                            infer_direction_for_trip_id(trip.get_trip_id()),
-                            chrono::UTC.timestamp(
-                                stop_time_update.get_arrival().get_time(), 0)));
-                    }
-                }
-            }
-        }
-    }
-
-    return upcoming;
-}
+mod utils;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -81,7 +43,7 @@ fn main() {
 
     let feed = fetcher.fetch(use_cache).expect("unwrap fetched feed");
 
-    let upcoming = upcoming_trains("R", "R20", &feed);
+    let upcoming = utils::upcoming_trains("R", "R20", &feed);
 
     for &(ref direction, ref ts) in upcoming.iter() {
         println!("{:?} {}", direction, ts.with_timezone(&tz));
---
+++ src/utils.rs
@@ -0,0 +1,44 @@
+extern crate chrono;
+
+use chrono::TimeZone;
+
+use gtfs_realtime;
+
+#[derive(Debug)]
+pub enum Direction {
+    UPTOWN,
+    DOWNTOWN,
+}
+
+fn infer_direction_for_trip_id(trip_id: &str) -> Direction {
+    // TODO(mrjones): Read the NYCT extension and determine this properly
+    let trip_id: String = trip_id.to_string();
+    let lastchar: String = trip_id.chars().rev().take(1).collect();
+    return match lastchar.as_ref() {
+        "N" => Direction::UPTOWN,
+        "S" => Direction::DOWNTOWN,
+        chr => panic!("Unexpcted direction {}", chr),
+    }
+}
+
+pub fn upcoming_trains(route: &str, stop_id: &str, feed: &gtfs_realtime::FeedMessage) -> Vec<(Direction, chrono::datetime::DateTime<chrono::UTC>)> {
+    let mut upcoming = Vec::new();
+    for entity in feed.get_entity() {
+        if entity.has_trip_update() {
+            let trip_update = entity.get_trip_update();
+            let trip = trip_update.get_trip();
+            if trip.get_route_id() == route {
+                for stop_time_update in trip_update.get_stop_time_update() {
+                    if stop_time_update.get_stop_id() == stop_id {
+                        upcoming.push((
+                            infer_direction_for_trip_id(trip.get_trip_id()),
+                            chrono::UTC.timestamp(
+                                stop_time_update.get_arrival().get_time(), 0)));
+                    }
+                }
+            }
+        }
+    }
+
+    return upcoming;
+}
