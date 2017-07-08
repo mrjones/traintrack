7b8293781b6b305c5fecc5bdbb6241004af0576d
Matt Jones <jonesmr@gmail.com>
2017-07-08 18:38:36 +0000

    make title a link

--- webclient/src/bootstrap.tsx
+++ webclient/src/bootstrap.tsx
@@ -302,18 +302,24 @@ ReactDOM.render(
     <Helmet>
       <meta name="viewport" content="initial-scale=1" />
     </Helmet>
-    <div className="app_title"><span className="first">Train</span><span className="second">Track</span></div>
-    <div className="app_content">
-      <ReactGAnalytics.BrowserRouter id="UA-102249880-1">
-        <ReactRouter.Switch>
-          <ReactRouter.Route path='/app/lines' component={LinePickerRouterWrapper}/>
-          <ReactRouter.Route path='/app/line/:lineId' component={LineViewRouterWrapper}/>
-          <ReactRouter.Route path='/app/station/:initialStationId' component={OneStationViewWrapperForRouter} />
-          <ReactRouter.Route path='/app/train/:trainId' component={TrainItineraryWrapper}/>
-          <ReactRouter.Route path='/app' component={OneStationViewWrapperForRouter}/>
-          <ReactRouter.Route path='/' component={OneStationViewWrapperForRouter}/>
-        </ReactRouter.Switch>
-      </ReactGAnalytics.BrowserRouter>
+    <ReactRouter.BrowserRouter>
+      <div className="app">
+        <div className="app_title">
+          <ReactRouter.Link to={`/app`}>
+            <span className="first">Train</span><span className="second">Track</span>
+          </ReactRouter.Link>
+        </div>
+        <div className="app_content">
+          <ReactRouter.Switch>
+            <ReactRouter.Route path='/app/lines' component={LinePickerRouterWrapper}/>
+            <ReactRouter.Route path='/app/line/:lineId' component={LineViewRouterWrapper}/>
+            <ReactRouter.Route path='/app/station/:initialStationId' component={OneStationViewWrapperForRouter} />
+            <ReactRouter.Route path='/app/train/:trainId' component={TrainItineraryWrapper}/>
+            <ReactRouter.Route path='/app' component={OneStationViewWrapperForRouter}/>
+            <ReactRouter.Route path='/' component={OneStationViewWrapperForRouter}/>
+          </ReactRouter.Switch>
+      </div>
     </div>
+    </ReactRouter.BrowserRouter>
   </div>,
   document.getElementById('tt_app'));
