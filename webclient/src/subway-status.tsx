4a7a1042a677854bbdad5951b0c480461c47332b
Matt Jones <jonesmr@gmail.com>
2019-06-05 20:27:53 +0000

    Cleaned up service status UI

--- webclient/src/subway-status.tsx
+++ webclient/src/subway-status.tsx
@@ -5,19 +5,41 @@ class SubwayStatusProps {
   public status: proto.ISubwayStatusMessage[];
 }
 
-export class SubwayStatus extends React.Component<SubwayStatusProps, any> {
+class SubwayStatusState {
+  public expanded: boolean;
+};
+
+export class SubwayStatus extends React.Component<SubwayStatusProps, SubwayStatusState> {
   constructor(props: SubwayStatusProps) {
     super(props);
+
+    this.state = {
+      expanded: false,
+    };
   }
 
   public render(): JSX.Element {
+    if (!this.props.status || this.props.status.length == 0) {
+      return null;
+    }
+
+    let description = this.props.status.length + " service status messages";
+
+    if (!this.state.expanded) {
+      return <div className="pubTime"><a href="#" onClick={this.toggleExpanded.bind(this)}>{description}</a></div>;
+    }
+
     let lis = this.props.status.map((msg: proto.ISubwayStatusMessage) => {
       let lines = msg.affectedLine.map((line: proto.AffectedLineStatus) => {
-        return line.line + (line.direction == proto.Direction.UPTOWN ? "U" : "D");
+        return line.line + (line.direction == proto.Direction.UPTOWN ? "\u2191" : "\u2193");
       }).join(" ");
-      return <li>{lines} {msg.summary}</li>;
+      return <li><strong>{lines}</strong> {msg.summary}</li>;
     });
 
-    return <div>Subway Status: <ul>{lis}</ul></div>
+    return <div className="pubTime"><a href="#" onClick={this.toggleExpanded.bind(this)}>{description}</a><ul>{lis}</ul></div>
+  }
+
+  private toggleExpanded() {
+    this.setState({expanded: !this.state.expanded});
   }
 }
